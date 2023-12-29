8d8934f4202f38606ecdb7719ddc72498c31ddaa
junichi <tmdjnch0901@gmail.com>
2023-12-29 11:32:03 +0900

    add home

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <header>
+        <title>jun01tのポートフォリオ</title>
+    </header>
+    <div class="text-center">
+        <h1 class="h-16 pt-4 text-2xl text-bold bg-gray-600 text-white">jun01tのポートフォリオ</h1>
+    </div>
+</template>
