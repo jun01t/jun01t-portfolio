a9dcd770967d57da733fb1270000d51d23fbba3b
junichi <tmdjnch0901@gmail.com>
2023-12-30 14:14:51 +0900

    modify home

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,89 @@
 <template>
     <header>
-        <title>jun01tのポートフォリオ</title>
+        <title>jun01t's Portfolio</title>
     </header>
-    <div class="text-center">
-        <h1 class="h-16 pt-4 text-2xl text-bold bg-gray-600 text-white">jun01tのポートフォリオ</h1>
+    <div class="text-center font-bold font-serif pb-16">
+        <h1 class="h-16 pt-3 text-3xl text-bold bg-gray-600 text-white">jun01t's Portfolio</h1>
+        <div>
+            <div class="flex w-36 ml-auto mr-auto mt-8">
+                <img class="w-5 h-5 mt-8 mr-4" src="/asset/img/icons8-プログラマー-50.png" alt="プログラマー-50" />
+                <div class="text-2xl pt-8">職業</div>
+            </div>
+            <div class="text-xs text-gray-300">
+                <a target="_blank" href="https://icons8.com/icon/53381/%E9%96%8B%E7%99%BA%E8%80%85">プログラマー</a>
+                アイコン by <a target="_blank" href="https://icons8.com">Icons8</a>
+            </div>
+            <ul class="pt-4 text-xl font-normal text-gray-600">
+                <li class="pt-2">ソフトウェアエンジニア</li>
+            </ul>
+        </div>
+        <div>
+            <div class="flex w-52 ml-auto mr-auto mt-4">
+                <img class="w-5 h-5 mt-5 mr-4" src="/asset/img/icons8-ウェブアプリ-64.png" alt="ウェブアプリ-64" />
+                <div class="text-2xl pt-4">フレームワーク</div>
+            </div>
+            <div class="text-xs text-gray-300">
+                <a target="_blank"
+                    href="https://icons8.com/icon/gWFtpuoNEmKs/%E3%82%A6%E3%82%A7%E3%83%96%E3%82%A2%E3%83%97%E3%83%AA">ウェブアプリ</a>
+                アイコン by <a target="_blank" href="https://icons8.com">Icons8</a>
+            </div>
+            <ul class="text-xl pt-4 font-normal text-gray-600">
+                <li class="pt-2">Ruby on Rails</li>
+                <li class="pt-2">Vue.js</li>
+                <li class="pt-2">Spring Boot</li>
+            </ul>
+        </div>
+        <div>
+            <div class="flex w-24 ml-auto mr-auto mt-4">
+                <img class="w-5 h-5 mt-5 mr-4" src="/asset/img/icons8-プログラミング言語-50.png" alt="プログラミング言語-50" />
+                <div class="text-2xl pt-4">言語</div>
+            </div>
+            <div class="text-xs text-gray-300">
+                <a target="_blank"
+                    href="https://icons8.com/icon/22181/ruby%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0%E8%A8%80%E8%AA%9E">プログラミング言語</a>
+                アイコン by <a target="_blank" href="https://icons8.com">Icons8</a>
+            </div>
+            <ul class="text-xl pt-4 font-normal text-gray-600">
+                <li class="pt-2">Ruby</li>
+                <li class="pt-2">JavaScript</li>
+                <li class="pt-2">PHP</li>
+                <li class="pt-2">Java</li>
+                <li class="pt-2">HTML/CSS</li>
+            </ul>
+        </div>
+        <div>
+            <div class="flex w-24 ml-auto mr-auto mt-4">
+                <img class="w-5 h-5 mt-5 mr-4" src="/asset/img/icons8-データベース-50.png" alt="データベース-50" />
+                <div class="text-2xl pt-4">DB</div>
+            </div>
+            <div class="text-xs text-gray-300">
+                <a target="_blank"
+                    href="https://icons8.com/icon/1476/%E3%83%87%E3%83%BC%E3%82%BF%E3%83%99%E3%83%BC%E3%82%B9">データベース</a>
+                アイコン by <a target="_blank" href="https://icons8.com">Icons8</a>
+            </div>
+            <ul class="text-xl pt-4 font-normal text-gray-600">
+                <li class="pt-2">PostgreSQL</li>
+                <li class="pt-2">Oracle</li>
+                <li class="pt-2">MySQL</li>
+            </ul>
+        </div>
+        <div>
+            <div class="flex w-24 ml-auto mr-auto mt-4">
+                <img class="w-8 h-8 mt-3 mr-4" src="/asset/img/icons8-アマゾンウェブサービス-32.png" alt="アマゾンウェブサービス-32" />
+                <div class="text-2xl pt-4">IaaS</div>
+            </div>
+            <div class="text-xs text-gray-300">
+                <a target="_blank"
+                    href="https://icons8.com/icon/VoXRGxL3ekkk/%E3%82%A2%E3%83%9E%E3%82%BE%E3%83%B3%E3%82%A6%E3%82%A7%E3%83%96%E3%82%B5%E3%83%BC%E3%83%93%E3%82%B9">アマゾンウェブサービス</a>
+                アイコン by <a target="_blank" href="https://icons8.com">Icons8</a>
+            </div>
+            <ul class="text-xl pt-4 font-normal text-gray-600">
+                <li class="pt-2">AWS</li>
+            </ul>
+        </div>
     </div>
+    <footer>
+        <h1 class="h-16 pt-3 text-3xl text-bold bg-gray-600 text-white text-center font-bold font-serif">jun01t's Portfolio
+        </h1>
+    </footer>
 </template>
